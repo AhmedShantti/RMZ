9cb4acbe3d294a7fd1a514d74e762331448db297
Ahmed Shantti <shantti04@gmail.com>
2026-07-29 15:33:23 +0300

    latest updates on migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260729_111016 from './20260729_111016';
+import * as migration_20260729_121316 from './20260729_121316';
 
 export const migrations = [
   {
-    up: migration_20260729_111016.up,
-    down: migration_20260729_111016.down,
-    name: '20260729_111016'
+    up: migration_20260729_121316.up,
+    down: migration_20260729_121316.down,
+    name: '20260729_121316'
   },
 ];
